17c982d5dc7c48ab438e9e8843244fc83d102827
JesseBennett-lab <j.bennett0505@gmail.com>
2026-03-04 15:47:14 +0200

    Update information

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -5,11 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.net.MalformedURLException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        var FirstName="Kyle"
+        var Surname='Adams'
+        var Adress="5 Rosemary Road,Table View"
+        var IsMale= true
+        var DayOfBirth="11"
+        var MonthOfBirth="October"
+        var YearOfBirth="2005"
+
+        
+
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
