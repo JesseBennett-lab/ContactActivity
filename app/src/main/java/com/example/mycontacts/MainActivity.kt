9d3fc6a02d003f26618468137326e55edbac9705
JesseBennett-lab <j.bennett0505@gmail.com>
2026-03-05 18:14:18 +0200

    added calculations

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -21,10 +21,19 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(
             "Hello",
-            "$firstName $surname your address is $address it is $isMale that you are male, " +
+            "$firstName $surname your address is $address it is $isMale that you are male " +
                     "you were born on $dayOfBirth $monthOfBirth $yearOfBirth"
         )
-        var
+        val age = 21
+        val yearsTill100 =100 - age
+        val ageInMonths =21 * 12
+        val ageInDays = 21 * 365
+        val remainder = 21 % 5
+
+        Log.d("RESULT", "Years until 100: $yearsTill100")
+        Log.d("RESULT", "Age in months: $ageInMonths")
+        Log.d("RESULT", "Age in days: $ageInDays")
+        Log.d("RESULT", "Remainder when divided by 5: $remainder")
 
                     setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
